1f97f75150ed1d1f3493a1bda941aa82ef4554a4
vadwwxz <vadwwxz@gmail.com>
2023-02-10 19:08:09 +0200

    Made task MedianOfTwoSortedArrays on Kotlin

--- src/main/java/hard/part1/MedianOfTwoSortedArraysJavaTask.java
+++ src/main/java/hard/part1/MedianOfTwoSortedArraysJavaTask.java
@@ -2,7 +2,7 @@ package hard.part1;
 
 import java.util.Arrays;
 
-/*
+/**
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
 
@@ -15,7 +15,7 @@ The overall run time complexity should be O(log (m+n)).
     Input: nums1 = [1,2], nums2 = [3,4]
     Output: 2.50000
     Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
- */
+*/
 public class MedianOfTwoSortedArraysJavaTask {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] arr = new int[nums1.length+nums2.length];
