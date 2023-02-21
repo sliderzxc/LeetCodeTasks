9b6eaf1f39885cc338ac18b6d482967c6a03ace2
vadwwxz <vadwwxz@gmail.com>
2023-02-21 18:16:19 +0200

    Made task RegularExpressionMatching on Java

---
+++ src/main/java/medium/part1/RegularExpressionMatchingJavaTask.java
@@ -0,0 +1,46 @@
+package medium.part1;
+
+/**
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+    Example 1:
+    Input: s = "aa", p = "a"
+    Output: false
+    Explanation: "a" does not match the entire string "aa".
+
+    Example 2:
+    Input: s = "aa", p = "a*"
+    Output: true
+    Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes"aa".
+
+    Example 3:
+    Input: s = "ab", p = ".*"
+    Output: true
+    Explanation: ".*" means "zero or more (*) of any character (.)".
+ */
+public class RegularExpressionMatchingJavaTask {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') && dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
