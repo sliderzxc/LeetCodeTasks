286209f5305a8e22b07de71dfaebc19ee7bee9ba
vadwwxz <vadwwxz@gmail.com>
2023-02-25 23:33:30 +0200

    Made task LongestPalindromicSubstring on Kotlin

---
+++ src/main/kotlin/medium/part1/LongestPalindromicSubstringKotlinTask.kt
@@ -0,0 +1,43 @@
+package medium.part1
+
+/**
+ * Given a string s, return the longest palindromic substring in s.
+ * <p>
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * <p>
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+class LongestPalindromicSubstringKotlinTask {
+    fun longestPalindrome(s: String?): String? {
+        if (s == null || s.length < 1) {
+            return ""
+        }
+        var start = 0
+        var end = 0
+        for (i in 0 until s.length) {
+            val len1 = expandAroundCenter(s, i, i)
+            val len2 = expandAroundCenter(s, i, i + 1)
+            val len = Math.max(len1, len2)
+            if (len > end - start) {
+                start = i - (len - 1) / 2
+                end = i + len / 2
+            }
+        }
+        return s.substring(start, end + 1)
+    }
+
+    private fun expandAroundCenter(s: String, left: Int, right: Int): Int {
+        var L = left
+        var R = right
+        while (L >= 0 && R < s.length && s[L] == s[R]) {
+            L--
+            R++
+        }
+        return R - L - 1
+    }
+}
