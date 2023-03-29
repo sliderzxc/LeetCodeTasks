fa9d145b0a65dfa19f6888ec13324b76f97633f8
vadwwxz <vadwwxz@gmail.com>
2023-03-29 23:33:51 +0300

    Made task LetterCombinationsOfPhoneNumber on Kotlin

--- src/main/java/medium/part2/LetterCombinationsOfPhoneNumberJavaTask.java
+++ src/main/java/medium/part2/LetterCombinationsOfPhoneNumberJavaTask.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static javax.swing.UIManager.put;
-
 /**
  * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
  * Return the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given below.
