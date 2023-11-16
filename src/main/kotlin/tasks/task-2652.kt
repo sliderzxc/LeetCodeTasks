0244e7b7ddfa5a8e300dbc363b2d7b360c27becf
vadwwxz <vadwwxz@gmail.com>
2023-11-16 18:46:46 +0200

    task-2652

---
+++ src/main/kotlin/tasks/task-2652.kt
@@ -0,0 +1,19 @@
+package tasks
+
+/**
+ * Solution class for the problem - https://leetcode.com/problems/sum-multiples/
+ *
+ * This class provides a method to calculate the sum of multiples
+ * of 3, 5, and 7 up to a given number 'n'.
+ */
+class Solution {
+    fun sumOfMultiples(n: Int): Int {
+        var result = 0
+        for (i in 1..n) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                result += i
+            }
+        }
+        return result
+    }
+}
